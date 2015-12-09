6f9feec6d8a1fc67ef223bde6cb82103272f4b27
John Kee <jkee@rosettastone.com>
2015-12-09 17:48:43 -0600

    endpoints are now (mostly) all under /blanco/api/customizations.  moved test S3 write under /test-write endpoint

--- main.js
+++ main.js
@@ -13,20 +13,8 @@ var AWS = require('aws-sdk');
 
 
 var app = express();
-var s3 = new AWS.S3();
-AWS.config.loadFromPath('./config.json');
-
-//
-// Test
-//
-s3.createBucket({Bucket: 'elasticbeanstalk-us-west-2-393679320285'}, function() {
-  var params = {Bucket: 'elasticbeanstalk-us-west-2-393679320285', Key: 'configs/testKey', Body: 'Hello!'};
-  s3.putObject(params, function(err, data) {
-      if (err)
-          console.log(err)
-      else       console.log("Successfully uploaded data to configs/newTestKey");
-   });
-});
+//var s3 = new AWS.S3();
+//AWS.config.loadFromPath('./config.json');
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,25 +12,44 @@ router.get('/about', function(req, res) {
   res.send('Blanco - Customization Settings Manager for White-Label of Aria');
 });
 
-router.get('/config/all', function(req, res) {
+router.get('/test-write', function(req, res) {
+  //
+  // Test writing to S3
+  //
+  s3.createBucket({Bucket: 'elasticbeanstalk-us-west-2-393679320285'}, function() {
+    var params = {Bucket: 'elasticbeanstalk-us-west-2-393679320285', Key: 'configs/testKey', Body: 'Hello!'};
+    s3.putObject(params, function(err, data) {
+        if (err) {
+            console.log(err)
+            res.send('Error writing to S3 - ' + err);
+        }
+        else
+        {
+          console.log("Successfully uploaded data to configs/newTestKey");
+          res.send('wrote test record.  Check S3 congigs/testKey');
+        }
+     });
+  });
+});
+
+router.get('/blanco/api/customizations/all', function(req, res) {
   res.send('This will be to retrieve all config filenames');
 });
 
-router.get('/white-label/sparkly', function(req, res) {
-  var img = fs.readFileSync('images/sparkly.jpg');
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
-});
-
-router.get('/config', function(req, res) {
+router.get('/blanco/api/customizations', function(req, res) {
   res.send('This will be to retrieve a given config JSON');
 });
 
-router.post('/config', function(req, res) {
+// Dummy service to retrieve json
+router.get('/blanco/api/customizations/abcd-1234', function(req, res) {
+  res.send('{"id":"abcd-1234","name":"Jones Customization File","Button-color":"#339933"}');
+});
+
+router.post('/blanco/api/customizations', function(req, res) {
   res.send('This will be to save a given config JSON');
 });
 
-router.delete('/config', function(req, res) {
+router.delete('/blanco/api/customizations', function(req, res) {
   res.send('This will be to delete a given config JSON');
 });
 
