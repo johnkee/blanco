0d1cfa518045c4d5c1ad368537c112a37aa13af0
John Kee <jkee@rosettastone.com>
2015-12-10 15:13:59 -0600

    save and delete (POST and DELETE) working for customizations endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,28 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
+var bodyParser = require('body-parser');
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 
+app.use(bodyParser.json());
+
+var BUCKET =  'elasticbeanstalk-us-west-2-393679320285';
+var folder =  'customizations';
+var status =  'status';
+var success = 'success';
+var error =    'error';
+
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/about', function(req, res) {
+app.get('/about', function(req, res) {
   res.send('Blanco - Customization Settings Manager for White-Label of Aria');
 });
 
-router.get('/test-write', function(req, res) {
+app.get('/test-write', function(req, res) {
   //
   // Test writing to S3
   //
@@ -33,26 +43,53 @@ router.get('/test-write', function(req, res) {
   //});
 });
 
-router.get('/blanco/api/customizations/all', function(req, res) {
+app.get('/blanco/api/customizations/all', function(req, res) {
   res.send('This will be to retrieve all config filenames');
 });
 
-router.get('/blanco/api/customizations', function(req, res) {
+app.get('/blanco/api/customizations', function(req, res) {
   res.send('This will be to retrieve a given config JSON');
 });
 
 // Dummy service to retrieve json
-router.get('/blanco/api/customizations/abcd-1234', function(req, res) {
+app.get('/blanco/api/customizations/abcd-1234', function(req, res) {
   res.send('{"id":"abcd-1234","name":"Jones Customization File","Button-color":"#339933"}');
 });
 
-router.post('/blanco/api/customizations', function(req, res) {
-  res.send('This will be to save a given config JSON');
+app.post('/blanco/api/customizations', function(req, res) {
+  var submission = {};
+
+  submission = req.body;
+  //submission.id = req.body.id;
+  //submission.data = req.body.data;
+  //submission.stuff = req.body.other;
+  
+  var params = {Bucket: BUCKET, Key: folder + '/' + submission.id, Body: JSON.stringify(submission)};
+  s3.putObject(params, function(err, data) {
+    if (err) {
+      console.log(err);
+      console.dir(submission);
+      res.send('Error saving file: ' + err);
+    }
+    else
+    {
+      console.log("Successfully uploaded data to " + folder + "/" + submission.id);
+      res.send('{"' + status + '":"' + success + '","id":"' + submission.id + '"}');
+    }
+  });
 });
 
-router.delete('/blanco/api/customizations', function(req, res) {
-  res.send('This will be to delete a given config JSON');
+app.delete('/blanco/api/customizations/:id', function(req, res) {
+  var params = {Bucket: BUCKET, Key: folder + '/' + req.params.id};
+  s3.deleteObject(params, function(err, data) {
+    if (err) console.log(err, err.stack);
+    else
+    {
+      console.log(req.params.id + ' deleted.');
+      res.send('{"' + status + '":"' + success + '","message":"' + req.params.id + '" deleted"}');
+    }
+  });
 });
 
 
-module.exports = router;
+module.exports = app;
